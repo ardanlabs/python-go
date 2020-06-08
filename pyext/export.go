0b03f3e45fe6e90936b9ea8a3004e0ba30853537
William Kennedy <bill@ardanlabs.com>
2020-06-08 11:56:34 -0400

    minor format changes

--- pyext/export.go
+++ pyext/export.go
@@ -5,11 +5,9 @@ import "C"
 //export verify
 func verify(root *C.char) *C.char {
 	rootDir := C.GoString(root)
-	err := CheckSignatures(rootDir)
-	if err == nil {
+	if err := CheckSignatures(rootDir); err == nil {
 		return nil
 	}
-
 	str := C.CString(err.Error())
 	return str
 }
