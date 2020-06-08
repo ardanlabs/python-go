de164849ae51db35871fbc597ce89fb1a27b3821
Miki Tebeka <miki.tebeka@gmail.com>
2020-06-08 19:58:43 +0300

    fix "undefined: err" in export.go

--- pyext/export.go
+++ pyext/export.go
@@ -5,9 +5,11 @@ import "C"
 //export verify
 func verify(root *C.char) *C.char {
 	rootDir := C.GoString(root)
-	if err := CheckSignatures(rootDir); err == nil {
+	err := CheckSignatures(rootDir)
+	if err == nil {
 		return nil
 	}
+
 	str := C.CString(err.Error())
 	return str
 }
