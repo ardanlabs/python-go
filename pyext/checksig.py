3685251440ac8a2d4597585f2b7eac18e88eab82
Miki Tebeka <miki.tebeka@gmail.com>
2020-06-11 21:32:19 +0300

    so name

--- pyext/checksig.py
+++ pyext/checksig.py
@@ -1,12 +1,13 @@
 """Parallel check of files digital signature"""
 
 import ctypes
+from distutils.sysconfig import get_config_var
 from pathlib import Path
-
 
 # Load function and set its signature
 here = Path(__file__).absolute().parent
-so_file = here / '_checksig.so'
+ext_suffix = get_config_var('EXT_SUFFIX')
+so_file = here / ('_checksig' + ext_suffix)
 so = ctypes.cdll.LoadLibrary(so_file)
 verify = so.verify
 verify.argtypes = [ctypes.c_char_p]
