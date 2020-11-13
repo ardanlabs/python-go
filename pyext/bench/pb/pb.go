36d97a43f9d68ff1453d4abbeebd6ff1a3d7878f
William Kennedy <bill@ardanlabs.com>
2020-11-13 08:18:05 -0500

    fixing module issues

---
+++ pyext/bench/pb/pb.go
@@ -0,0 +1,9 @@
+// Package pb exists to prevent the Go module tooling from
+// failing with this error.
+//
+// go: finding module for package github.com/ardanlabs/python-go/pyext/bench/pb
+// github.com/ardanlabs/python-go/pyext/bench imports
+// github.com/ardanlabs/python-go/pyext/bench/pb: module
+// github.com/ardanlabs/python-go/pyext/bench@latest found (v0.0.0-20201113051146-dd49aa664084),
+// but does not contain package github.com/ardanlabs/python-go/pyext/bench/pb
+package pb
