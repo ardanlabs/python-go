8e36cb356f87dcb531f707e53bf74ee160028dd8
Miki Tebeka <miki.tebeka@gmail.com>
2020-11-15 20:29:11 +0200

    single test file

--- sqlite/trades/trades_test.go
+++ sqlite/trades/trades_test.go
@@ -1,7 +1,9 @@
 package trades_test
 
 import (
+	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"testing"
 	"time"
 
@@ -59,4 +61,32 @@ func BenchmarkAdd(b *testing.B) {
 		err := db.Add(trade)
 		require.NoError(err)
 	}
+}
+
+func ExampleDB() {
+	dbFile := "/tmp/db-test" + time.Now().Format(time.RFC3339)
+	db, err := trades.NewDB(dbFile)
+	if err != nil {
+		fmt.Println("ERROR: create -", err)
+		return
+	}
+	defer db.Close()
+
+	const count = 10_000
+	for i := 0; i < count; i++ {
+		trade := trades.Trade{
+			Time:   time.Now(),
+			Symbol: "AAPL",
+			Price:  rand.Float64() * 200,
+			IsBuy:  i%2 == 0,
+		}
+		if err := db.Add(trade); err != nil {
+			fmt.Println("ERROR: insert - ", err)
+			return
+		}
+	}
+
+	fmt.Printf("inserted %d records\n", count)
+	// Output:
+	// inserted 10000 records
 }
